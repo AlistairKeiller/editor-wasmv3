005de19b8c5979ab6aaec2179b554388bc31d2ee
Alistair Keiller <alistair@keiller.net>
2023-02-15 00:19:07 -0800

    just do build target

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ cmake -G Ninja -S llvm-project/llvm -B build \
     -DLLVM_EXTERNAL_EDITOR_SOURCE_DIR="$(pwd)" \
     -DLLVM_PARALLEL_LINK_JOBS=2 \
     # -DLLVM_ENABLE_PROJECTS="clang;lld" \
-ninja -C build
+ninja -C build -- build
