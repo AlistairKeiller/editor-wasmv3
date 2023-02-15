d23ef2ae9e8a7a53937ab926fa4d4fdd7b250c7e
Alistair Keiller <alistair@keiller.net>
2023-02-15 10:16:16 -0800

    test

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ set -e
 sudo apt install -y ninja-build
 
 cmake -G Ninja -S llvm-project/llvm -B build \
-    -DCMAKE_BUILD_TYPE=Release \
     -DLLVM_EXTERNAL_PROJECTS="editor" \
     -DLLVM_EXTERNAL_EDITOR_SOURCE_DIR="$(pwd)" \
-    -DLLVM_PARALLEL_LINK_JOBS=2 \
+    -DLLVM-ENABLE-PROJECTS="clang;build" \
+    # -DLLVM_PARALLEL_LINK_JOBS=2 \
+    # -DCMAKE_BUILD_TYPE=Release \
     # -DLLVM_ENABLE_PROJECTS="clang;lld" \
 ninja -C build -- build
