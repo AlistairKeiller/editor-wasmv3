d9e963b054ee19d4f7a4ae130b13827a10349af2
Alistair Keiller <alistair@keiller.net>
2023-02-14 23:41:12 -0800

    test

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -e
 
 ls
 
-cmake -G Ninja -S llvm-project/llvm -B build \
+cmake -G Ninja -S llvm-project\llvm -B build \
     -DCMAKE_BUILD_TYPE=Release \
     # -DLLVM_ENABLE_PROJECTS="clang;lld" \
