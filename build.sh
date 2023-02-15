b5581659e240b830feec405528c805e2e7276b69
Alistair Keiller <alistair@keiller.net>
2023-02-15 00:18:09 -0800

    limit parallel jobs to 2

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ cmake -G Ninja -S llvm-project/llvm -B build \
     -DCMAKE_BUILD_TYPE=Release \
     -DLLVM_EXTERNAL_PROJECTS="editor" \
     -DLLVM_EXTERNAL_EDITOR_SOURCE_DIR="$(pwd)" \
+    -DLLVM_PARALLEL_LINK_JOBS=2 \
     # -DLLVM_ENABLE_PROJECTS="clang;lld" \
 ninja -C build
