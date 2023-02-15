f1591a3fabcccf8d166a61f2c907c4915f021902
Alistair Keiller <alistair@keiller.net>
2023-02-15 10:24:03 -0800

    test

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cmake -G Ninja -S llvm-project/llvm -B build \
     -DCMAKE_BUILD_TYPE=Release \
     -DLLVM_EXTERNAL_PROJECTS="build" \
     -DLLVM_EXTERNAL_BUILD_SOURCE_DIR="$(pwd)" \
-    -LLVM_ENABLE_PROJECTS="clang;build" \
+    -DLLVM_ENABLE_PROJECTS="clang;build" \
     # -DLLVM_PARALLEL_LINK_JOBS=2 \
     # -DLLVM_ENABLE_PROJECTS="clang;lld" \
 ninja -C build -- build
