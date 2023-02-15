30738700983b9a367d5261256845d517b925f08b
Alistair Keiller <alistair@keiller.net>
2023-02-14 23:55:25 -0800

    add external

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ sudo apt install -y ninja-build
 
 cmake -G Ninja -S llvm-project/llvm -B build \
     -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_EXTERNAL_PROJECTS="editor" \
+    -DLLVM_EXTERNAL_EDITOR_SOURCE_DIR="$(pwd)" \
     # -DLLVM_ENABLE_PROJECTS="clang;lld" \
